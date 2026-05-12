52857e261dc2cfb37eb880bae657cb562d4493c1
galaxiasssss <vitororbe@gmail.com>
2026-05-11 22:38:02 -0300

    86ahd8f02 - implementacao dos metodos getById, put, post e delete no firebase usando cloud firestore e validacao com postman

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -14,18 +14,26 @@ type User = { //estamos os tipos de dados
   data_nascimento: Date;
   email: string;
 };
- //definimos que id comeca com 0 (bem basico)
-let usuarios: User[] = []; //aqui temos uma lista de usuarios 
 
 export class UsersController {
-    static getAll (req: Request, res: Response) {
-      res.send(usuarios);
+    static async getAll (req: Request, res: Response) {
+      const snapshot = await getFirestore().collection("users").get(); //aqui estamos pegando a colecao de usuarios do firestore e usando o metodo get para obter os dados
+      const users = snapshot.docs.map(doc => { //
+        return {
+          id: doc.id,
+          ...doc.data()
+        } 
+      }); 
+      res.send(users);
     }
 
-    static getById (req: Request, res: Response) { //esse metodo get é para retornar o usuario especifico por id
-      let userId = Number(req.params.id); //pega o id do parametro da url
-      let user = usuarios.find(user => user.id === userId); //declara a variavel user e coloca o valor do usuario igual ao id do parametro 
-      res.send(user); //retorna o usuario buscado 
+    static async getById (req: Request, res: Response) { //esse metodo get é para retornar o usuario especifico por id
+      let userId = req.params.id; //pega o id do parametro da url
+      const doc = await getFirestore().collection("users").doc(userId).get(); //aqui estamos pegando a colecao de usuarios do firestore, usando o doc para pegar os dados/documentos e o get para mostrar os dados pelo id
+      res.send({ 
+        id: doc.id, 
+        ...doc.data()
+      }); //retorna o usuario buscado 
     }
 
     static async save (req: Request, res: Response) { //esse é o metodo http que cria recebe a solicitacao do usuario
@@ -37,22 +45,26 @@ export class UsersController {
     }
 
     static update (req: Request, res: Response) {
-        let userId = Number(req.params.id); //obteve o id do usuario pelo parametro
-        let user = req.body; // pegou as informacoes do corpo
-        let indexOf = usuarios.findIndex((_user: User) => _user.id === userId); //procurou o index do usuario pelo id
-        usuarios[indexOf].nome = user.nome; //atualizou o nome do usuario
-        usuarios[indexOf].idade = user.idade; //atualizou a idade do usuario
-        usuarios[indexOf].data_nascimento = user.data_nascimento; //atualizou a data de nascimento do usuario
-        usuarios[indexOf].email = user.email; //atualizou o email do usuario
+        let userId = req.params.id; //obteve o id do usuario pelo parametro
+        let user = req.body as User; // pegou as informacoes do corpo
+         
+        getFirestore().collection("users").doc(userId).set({
+          nome: user.nome,
+          idade: user.idade,
+          data_nascimento: user.data_nascimento,
+          email: user.email
+        });
+
         res.send({
             message: "Usuario atyalizado com sucesso",
         });
 }
 
     static delete (req: Request, res: Response) {
-        let userId = Number(req.params.id); //pega id do usuario pelo parametro
-        let indexOf = usuarios.findIndex((user: User) => user.id === userId); //procura o usuario pelo id e retorna o index
-        usuarios.splice(indexOf, 1); //splice é o metodo que remove o usuarios do array, o index é a posicao dele e o (, 1) remove um elemento
+        let userId = req.params.id;
+        
+        getFirestore().collection("users").doc(userId).delete();
+
         res.send({
             message: "Usuarios deletado com sucesso", 
         })
