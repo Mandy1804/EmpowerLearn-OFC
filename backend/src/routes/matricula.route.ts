1d8367aea6d305cd1fc985e8b259f2725fd1bb2e
galaxiasssss <vitororbe@gmail.com>
2026-06-25 23:49:11 -0300

    corrige erro de aluno se matricular

--- backend/src/routes/matricula.route.ts
+++ backend/src/routes/matricula.route.ts
@@ -1,11 +1,30 @@
-import express from 'express';
-import { MatriculaController } from '../controllers/matricula.controller';
-import asyncHandler from 'express-async-handler';
-import { permitir } from '../middlewares/permissao.middleware';
+import express from "express";
+import { MatriculaController } from "../controllers/matricula.controller";
+import asyncHandler from "express-async-handler";
+import { permitir } from "../middlewares/permissao.middleware";
 
 export const matriculaRoutes = express.Router();
 
-matriculaRoutes.get('/matriculas', permitir('aluno', 'professor', 'admin'), asyncHandler(MatriculaController.getAll));
-matriculaRoutes.get('/matriculas/:id', permitir('aluno', 'professor', 'admin'), asyncHandler(MatriculaController.getById));
-matriculaRoutes.post('/matriculas', permitir('admin'), asyncHandler(MatriculaController.save));
-matriculaRoutes.delete('/matriculas/:id', permitir('admin'), asyncHandler(MatriculaController.delete));
+matriculaRoutes.get(
+  "/matriculas",
+  permitir("aluno", "professor", "admin"),
+  asyncHandler(MatriculaController.getAll),
+);
+
+matriculaRoutes.get(
+  "/matriculas/:id",
+  permitir("aluno", "professor", "admin"),
+  asyncHandler(MatriculaController.getById),
+);
+
+matriculaRoutes.post(
+  "/matriculas",
+  permitir("aluno", "admin"),
+  asyncHandler(MatriculaController.save),
+);
+
+matriculaRoutes.delete(
+  "/matriculas/:id",
+  permitir("admin"),
+  asyncHandler(MatriculaController.delete),
+);
